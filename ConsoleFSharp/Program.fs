f6037d895e4b7edbf83418e7415c533c81561d26
Fabricio Cordeiro <fabriciojose3263@outlook.com>
2022-04-01 02:53:40 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleFSharp/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+open LibraryCSharp
+open LibraryVisualBasic
+
+printf " Digite um texto: "
+let texto = Console.ReadLine()
+Console.Clear()
+
+let CSharpEncrypt = Encrypt.Entrada(texto)
+let VisualBasicDecrypt = Decrypt.Saida(CSharpEncrypt).ToString()
+
+printfn "%s%s" " Texto obtido com F#: " texto
+printfn ""
+printfn "%s%s" " Texto cifrado com C#: " CSharpEncrypt
+printfn ""
+printfn "%s%s" " Texto decifrado com VB.NET: " VisualBasicDecrypt
+printfn ""
+
+let pause = Console.ReadKey()
---
+++ LibraryVisualBasic/Decrypt.vb
@@ -0,0 +1,33 @@
+Imports System.Security.Cryptography
+Imports System.Text
+Imports System.IO
+
+Public Class Decrypt
+    Public Shared Function Saida(cipherText As String)
+        Dim CipherTextBytes = Convert.FromBase64String(cipherText)
+
+        Dim Key = "3c75dd500c740df4a2e81f1e8dbc418f9eaf183d1b3714a1bf11bbd7c69c1e6f2a29fe76607d49b12096ef54a14c4cbaad3fc39ae2ec108d5a3777e6d5c2265f"
+        Dim InitialVectorBytes = Encoding.UTF8.GetBytes("OFRna73m*aze01xY")
+        Dim SaltBytes = Encoding.UTF8.GetBytes("43a7c01223db11653ce15c035cf05a454053be9669ce32f4af5aeee1f113058eb29dc816cbab67540d4da0566b495a71fa3cc932ef6c5d1f27dc7a4f165dcf2c")
+
+        Dim DerivedKey As New PasswordDeriveBytes(Key, SaltBytes, "SHA256", 1)
+        Dim KeyBytes = DerivedKey.GetBytes(32)
+        Dim SymmetricKey = Aes.Create()
+
+        Dim PlainTextBytes(CipherTextBytes.Length) As Byte
+
+        Dim Decryptor = SymmetricKey.CreateDecryptor(KeyBytes, InitialVectorBytes)
+
+        Dim MemStream As New MemoryStream(CipherTextBytes)
+        Dim CryptoStream As New CryptoStream(MemStream, Decryptor, CryptoStreamMode.Read)
+
+        Dim ByteCount
+        ByteCount = CryptoStream.Read(PlainTextBytes, 0, PlainTextBytes.Length)
+
+        MemStream.Close()
+        CryptoStream.Close()
+        SymmetricKey.Clear()
+
+        Return Encoding.UTF8.GetString(PlainTextBytes, 0, ByteCount)
+    End Function
+End Class
